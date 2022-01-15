98eb101ce2d3cea30d60106984902805fac703c0
ichikawariku3 <66261393+ichikawariku3@users.noreply.github.com>
2022-01-15 15:56:37 +0900

    commit

--- library.php
+++ library.php
@@ -8,10 +8,10 @@ function h($value) {
 function dbconnect() {
 	$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
-$server = $url["us-cdbr-east-05.cleardb.net"];
-$username = $url["b3be3df7a84238"];
-$password = $url["8a987262"];
-$db = substr($url["heroku_c4974c1348a806b"], 1);
+$server = $url["host"];//us-cdbr-east-05.cleardb.net
+$username = $url["user"];//b3be3df7a84238
+$password = $url["pass"];//8a987262
+$db = substr($url["path"], 1);//heroku_c4974c1348a806b
 
 	$conn = new mysqli($server, $username, $password, $db);
 	if (!$db) {
